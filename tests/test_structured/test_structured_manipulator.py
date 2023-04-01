b29a0ad4eef653819cf82f2e970e29b61feb3e6b
Abhishek Vijayakumar <vijayakumar.abhishek@gmail.com>
2023-04-01 16:18:01 -0400

    added initialization test

--- tests/test_structured/test_structured_manipulator.py
+++ tests/test_structured/test_structured_manipulator.py
@@ -1,7 +1,11 @@
+import pandas as pd
+
 from explainability.structured.structured_manipulator import \
     StructuredManipulator
 
 
 class TestStructuredManipulator:
-    def test_import(self):
-        assert True
+    def test_init(self):
+        s = StructuredManipulator(
+            pd.DataFrame(data={"col1": [1, 2], "col2": [3, 4]}),
+            label_column="col2")
