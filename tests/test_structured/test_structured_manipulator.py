1c22a4e0a74b362e21b978cef4887b00c528187c
Abhishek Vijayakumar <vijayakumar.abhishek@gmail.com>
2023-04-02 22:43:27 -0400

    added sort_values and train_test_split

--- tests/test_structured/test_structured_manipulator.py
+++ tests/test_structured/test_structured_manipulator.py
@@ -21,7 +21,8 @@ class TestStructuredManipulator:
         _ = sm.replace_random_values() \
             .duplicate_features() \
             .categorize() \
-            .split_category_value()
+            .split_category_value() \
+            .sort_values()
 
         _ = sm.trace
 
