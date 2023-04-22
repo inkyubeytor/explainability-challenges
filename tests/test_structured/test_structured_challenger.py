dfd8a7e325e802e5ed399dc04fe5b0fa6cd3ae36
Anna Cai <anana10c@gmail.com>
2023-04-22 17:46:43 -0400

    train models test

--- tests/test_structured/test_structured_challenger.py
+++ tests/test_structured/test_structured_challenger.py
@@ -1,5 +1,6 @@
 import copy
 import pandas as pd
+import numpy as np
 from sklearn.linear_model import LinearRegression
 
 from explainability.structured.core.structured_challenger import \
@@ -31,9 +32,33 @@ class TestSKChallenger:
         assert("base" in sc.challenges)
         assert("replace_values" in sc.challenges)
         assert("duplicate_features" in sc.challenges)
+        assert(len(sc.challenges) == 3)
 
         sm_base = sc.challenges["base"]
         sm_rv = sc.challenges["replace_values"]
         sm_df = sc.challenges["duplicate_features"]
         assert((sm_base.df.values != sm_rv.df.values).any())
         assert(len(sm_base.df.columns) + 1 == len(sm_df.df.columns))
+
+    def test_train_models(self):
+        model = LinearRegression()
+        sc = SimpleChallenger(model,
+                              pd.DataFrame(data={"col1": [1, 2, 3, 4, 5, 6],
+                                                 "col2": ['a', 'a', 'a', 'b', 'b', 'b'],
+                                                 "col3": [1, 1, 1, 1, 1, 1]}, ),
+                              label_column="col3")
+        sc.generate_challenges()
+        sc.train_models()
+        assert(len(sc.models) == 3)
+
+        for challenge in sc.challenges:
+            x_train, y_train, x_test, y_test = \
+                sc.challenges[challenge].train_test_split(test_proportion=0.34)
+            pipe = sc.models[challenge]
+            enc = pipe.named_steps["encoder"]
+            scal = pipe.named_steps["scaler"]
+            model = pipe.named_steps["model"]
+            x_train = scal.transform(enc.transform(x_train))
+            x_test = scal.transform(enc.transform(x_test))
+            assert(model.score(x_train, y_train) == 1)
+            assert(model.score(x_test, y_test) == 1)
