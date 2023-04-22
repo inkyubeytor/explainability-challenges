f0230783bd20b733bdd33fcd224854341c352c98
Anna Cai <anana10c@gmail.com>
2023-04-22 17:28:01 -0400

    generate challenges test

--- tests/test_structured/test_structured_challenger.py
+++ tests/test_structured/test_structured_challenger.py
@@ -17,8 +17,23 @@ class SimpleChallenger(SKChallenger):
 
 class TestSKChallenger:
     def test_init(self):
-
         model = LinearRegression()
         _ = SimpleChallenger(model,
-            pd.DataFrame(data={"col1": [1, 2], "col2": [3, 4]}),
-            label_column="col2")
+                             pd.DataFrame(data={"col1": [1, 2], "col2": [3, 4]}),
+                             label_column="col2")
+
+    def test_generate_challenges(self):
+        model = LinearRegression()
+        sc = SimpleChallenger(model,
+                              pd.DataFrame(data={"col1": [1, 2], "col2": [3, 4]}),
+                              label_column="col2")
+        sc.generate_challenges()
+        assert("base" in sc.challenges)
+        assert("replace_values" in sc.challenges)
+        assert("duplicate_features" in sc.challenges)
+
+        sm_base = sc.challenges["base"]
+        sm_rv = sc.challenges["replace_values"]
+        sm_df = sc.challenges["duplicate_features"]
+        assert((sm_base.df.values != sm_rv.df.values).any())
+        assert(len(sm_base.df.columns) + 1 == len(sm_df.df.columns))
